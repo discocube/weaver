f67007063ef19feaa91ffb3905fb54bce545d3fd
www.discocube.org <93198518+discocube@users.noreply.github.com>
2023-03-30 18:57:51 +0200

    Update defs.rs

--- src/graph/defs.rs
+++ src/graph/defs.rs
@@ -37,15 +37,21 @@ pub type SignedIdx = i32;
 pub type Yarn = Array2<Point>;
 pub type ZlevelNodesMap = HashMap<Point, Nodes>;
 pub type ZOrder = Vec<(Point, usize)>;
-use std::error::Error;
 use serde::Serialize;
+use std::error::Error;
+
+#[derive(Debug, Clone, Copy)]
+pub enum XY {
+    X,
+    Y,
+}
 
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "PascalCase")]
 struct Vector {
     x: i16,
     y: i16,
-    z: i16
+    z: i16,
 }
 
 #[derive(Clone, Debug)]
@@ -53,13 +59,13 @@ pub struct Weaver {
     pub data: Tour,
     lead: bool,
     min_xyz: Point,
-    order: u32, 
+    order: u32,
 }
 
 impl Weaver {
     pub fn new(mut data: YarnEnds, lead: bool, min_xyz: Point, order: u32) -> Weaver {
         let mut preallocated = Vec::with_capacity(order as usize);
-        preallocated.extend(data.drain(..)); 
+        preallocated.extend(data.drain(..));
         Weaver {
             data: preallocated,
             lead,
@@ -85,7 +91,7 @@ impl Weaver {
         // More steps to fulfill optimality than just doing what's correct.
         if lhs == self.data[self.data.len() - 1] && rhs == self.data[0] {
             self.data.reverse();
-        } else if !(lhs == self.data[0] && rhs == self.data[self.data.len() - 1]){
+        } else if !(lhs == self.data[0] && rhs == self.data[self.data.len() - 1]) {
             match (
                 self.data.iter().position(|&x| x == lhs).unwrap(),
                 self.data.iter().position(|&x| x == rhs).unwrap(),
@@ -102,7 +108,7 @@ impl Weaver {
     pub fn rotate_to_edge(cycle: &mut Tour, (lhs, rhs): ([i16; 3], [i16; 3])) {
         if lhs == cycle[cycle.len() - 1] && rhs == cycle[0] {
             cycle.reverse();
-        } else if !(lhs == cycle[0] && rhs == cycle[cycle.len() - 1]){
+        } else if !(lhs == cycle[0] && rhs == cycle[cycle.len() - 1]) {
             match (
                 cycle.iter().position(|&x| x == lhs).unwrap(),
                 cycle.iter().position(|&x| x == rhs).unwrap(),
@@ -129,37 +135,6 @@ impl Weaver {
         self.data.to_vec()
     }
 
-    pub fn create_3d_line_plot(&self) -> Result<(), Box<dyn std::error::Error>> {
-        use plotters::prelude::*;
-        let root = BitMapBackend::new("3d_plot.png", (640, 480)).into_drawing_area();
-        root.fill(&WHITE)?;
-    
-        let mut xx: Vec<i16> = Vec::new();
-        let mut yy: Vec<i16> = Vec::new();
-        let mut zz: Vec<i16> = Vec::new();
-    
-        for [x, y, z] in self.data.iter() {
-            xx.push(*x);
-            yy.push(*y);
-            zz.push(*z);
-        }
-    
-        let mut chart = ChartBuilder::on(&root)
-            .caption("3D Line Plot", ("Arial", 30).into_font())
-            .build_cartesian_3d(-100..100, -100..100, -100..100)?;
-    
-            chart.draw_series(LineSeries::new(
-                xx
-                    .iter()
-                    .zip(yy.iter())
-                    .zip(zz.iter())
-                    .map(|((x, y), z)| (*x as i32, *y as i32, *z as i32)),
-                &BLACK,
-            ))?;
-    
-        Ok(())
-    }
-
     /// Python script using plotly and pandas to display the solution from the .csv file produced by the function below:
     ///```
     /// import pandas as pd
@@ -169,16 +144,25 @@ impl Weaver {
     ///     df = pd.read_csv(file_path)
     ///     fig = px.line_3d(df, x='X', y='Y', z='Z')
     ///     fig.show()
-    /// 
+    ///
     ///```
     /// Save solution to file_path as a csv file where each axis X, Y, Z is a separate column.
     /// Structured for easy read and plotting using python and plotly (see above).
     pub fn save_to_csv(&self, file_path: &str) -> Result<(), Box<dyn Error>> {
-        let file = std::fs::File::create(&file_path)?;
+        let file = std::fs::File::create(file_path)?;
         let mut writer = csv::Writer::from_writer(file);
         self.data.iter().for_each(|[x, y, z]| {
-            writer.serialize(Vector{x:*x, y:*y, z:*z}).ok();
+            writer
+                .serialize(Vector {
+                    x: *x,
+                    y: *y,
+                    z: *z,
+                })
+                .ok();
         });
+        // Add first value to last to form a loop:
+        let [x, y, z] = self.data[0];
+        writer.serialize(Vector { x, y, z })?;
         writer.flush()?;
         Ok(())
     }
