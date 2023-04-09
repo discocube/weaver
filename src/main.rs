91e4c809a9b5c4c8cb1fae98c03e5e39dd634dea
www.discocube.org <93198518+discocube@users.noreply.github.com>
2023-04-09 06:50:06 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,29 +14,30 @@
 /// makes graph, solves it
 /// 1 (start with order 8 end at order 1,373,600) 100 in steps of two: [1, 3, 5, 7...]
 /// GOAL IS 8011618152. 8 BILLION FOR EACH PERSON ON THE EARTH. N = 1817.
+///                                                   
+///                                                   
+///                                                   
+///                      ◉―――――――◉                    
+///                     ╱│      ╱│                    
+///                    ◉―――⊖―――◉―│―◉                  
+///                    │ │╱│   │ │╱│                  
+///              ◉―――――│―◉―――――│―◉――――――――◉           
+///             ╱│     │╱│ │   │╱│ │     ╱│           
+///            ◉―――――――◉―――⊖―――◉―――⊖――――◉ │           
+///            │ │    ╱│ │╱   ╱│ │╱     │ │           
+///            │ ◉―――◉―――⊖―――◉―│―◉――――――│―◉           
+///            │╱    │ │╱│   │ │╱│      │╱            
+///            ◉―――――│―◉―――――│―◉――――――――◉             
+///                  │╱│ │   │╱│ │                    
+///                  ◉―――⊖―――◉―│―◉                    
+///                    │╱      │╱                     
+///                    ◉―――――――◉                      
+///                                                   
+///                                                   
+///                                         
 ///
 ///
-///           
-///                         ◉―――――――◉  
-///                        ╱│      ╱│  
-///                       ◉―――⊖―――◉―│―◉
-///                       │ │╱│   │ │╱│
-///                 ◉―――――│―◉―――――│―◉――――――――◉
-///                ╱│     │╱│ │   │╱│ │     ╱│  
-///               ◉―――――――◉―――✧―――◉―――◉――――◉―│   
-///               │ │    ╱│ │╱   ╱│ │╱     │ │  
-///               │ ◉―――◉―――✧―――◉―│―◉――――――│―◉  
-///               │╱    │ │╱│   │ │╱│      │╱   
-///               ◉―――――│―◉―――――│―◉――――――――◉                  
-///                     │╱│ │   │╱│ │  
-///                     ◉―――✧―――◉―│―◉  
-///                       │╱      │╱   
-///                       ◉―――――――◉          
-///
-///
-///
-///
-///
+///                                               
 /////////////////////////////////////////////////////////////////////////////
 extern crate blas_src;
 extern crate chrono;
@@ -44,15 +45,11 @@ extern crate rayon;
 
 pub mod graph;
 
-use graph::{
-    defs::*,
-    utils::certify::{is_hamiltonian_circuit, SequenceID},
-    utils::info::get_order_from_n,
-    weave,
-};
+use graph::{defs::*, utils::certify::SequenceID, utils::debug::get_current_date_time, weave};
 use std::{env, time::Instant};
 
 pub fn main() -> Result<(), &'static str> {
+    std::env::set_var("RUST_BACKTRACE", "1");
     let args: Vec<String> = env::args().collect();
     let n_start = args
         .get(1)
@@ -66,36 +63,35 @@ pub fn main() -> Result<(), &'static str> {
         .unwrap_or(n_start);
     let steps = args.get(3).and_then(|arg| arg.parse().ok()).unwrap_or(1);
     for level in (n_start..=n_end).step_by(steps) {
-        find_solution(level, false)?;
+        find_solutions(level, false)?;
     }
     Ok(())
 }
 
-pub fn find_solution(n: usize, _certify: bool) -> Result<Solution, &'static str> {
+// Solve on one or many by step or by steps. Time it and certify.
+pub fn find_solutions(n: usize, _certify: bool) -> Result<Solution, &'static str> {
+    let order = n.get_order_from_n();
+    if order > 1000000000 {
+        println!("{} | SOLVING ORDER ⭕️ {order}", get_current_date_time());
+    }
     let mut start: Instant = Instant::now();
-    let order = get_order_from_n(n);
-    let dur_make = Instant::now() - start;
-    start = Instant::now();
     let solution = weave::weave(n);
     let dur_solve = Instant::now() - start;
-    println!(
-        "| 🇳 {n:>4} | 🕗 MAKE: {} | ⭕️ {order:>10} | 🕗 SOLVE: {} |",
-        dur_make.as_secs_f32(),
-        dur_solve.as_secs_f32(),
-    );
-    start = Instant::now();
-    let seq_id = is_hamiltonian_circuit(
-        &solution,
-        order,
-        GraphGuide::get_max_absumv_from_n16(n),
-    );
-    let dur_certify = Instant::now() - start;
-    println!(
-        "| 🇳 {n:>4} | 🕗 MAKE: {} | ⭕️ {order:>10} | 🕗 SOLVE: {} | 📌 {seq_id:?} | 🕗 CERTIFY: {}",
-        dur_make.as_secs_f32(),
-        dur_solve.as_secs_f32(),
-        dur_certify.as_secs_f32()
-    );
-    assert_eq!(seq_id, SequenceID::HamCycle);
+    if order > 10000000 {
+        println!(
+            "| 🇳 {n:>4} | ⭕️ {order:>10} | 🕗 {:.10} |",
+            dur_solve.as_secs_f32(),
+        );
+    } else {
+        start = Instant::now();
+        let seq_id = solution.certify(order, n.get_max_absumv());
+        let dur_certify = Instant::now() - start;
+        println!(
+            "| 🇳 {n:>4} | ⭕️ {order:>10} | 🕗 SOLVE: {:.10} | 📌 {seq_id:?} | 🕗 CERTIFY: {:.10}",
+            dur_solve.as_secs_f32(),
+            dur_certify.as_secs_f32()
+        );
+        assert_eq!(seq_id, SequenceID::HamCycle);
+    }
     Ok(solution)
 }
