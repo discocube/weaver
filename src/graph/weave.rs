7a37b93022967fb69093c17399616986bd844d7e
www.discocube.org <93198518+discocube@users.noreply.github.com>
2023-04-10 19:26:17 +0200

    Update weave.rs

--- src/graph/weave.rs
+++ src/graph/weave.rs
@@ -1,4 +1,4 @@
-use super::defs::*;
+use super::{defs::*, ops::prelude::*};
 
 /// 🕸️ Weave a Hamiltonian cycle by building chains level by level bottom up until subtours of half is formed. Mirror chains to form cycles for subsequent joining of weft with each warp in the loom until only the weft remains.\
 ///
@@ -15,33 +15,27 @@ use super::defs::*;
 ///```
 ///pub fn weave(n: usize) -> Solution {
 ///
-///    // Spin initial hamchain for last/longest level from whence all solutions derive.
-///    let spool = Spool::spin(n.get_spool_size());  
-///
-///    // Convert to ndarray and assign to blue. Copy blue, reflect, translate and assign to red.
-///    let yarns = Yarns::colorize(spool);  
-///
 ///    // Init pin cushion with capacity provided by g.
-///    let mut pins: Cushion = Cushion::with_capacity(g.n);  
+///    let mut pins = PinCushion::with_capacity(n);
 ///
-///    // Init `pins` with specific capacity provided by g.
-///    let mut loom: Loom = Loom::with_capacity(n.get_loom_size()); 
+///    // Convert to ndarray & assign to blue. Copy/reflect/translate blue & assign to red.
+///    let yarns = Yarns::colorized(Spindle::spin(n.spool_size()));
 ///
 ///    // Iterate over the setting for each level: `zrow`, `size`, `color`
-///    n.get_zpos_size_color().iter().for_each(|&((zrow, size), color)| {
+///    n.zrow_color_size().iter().for_each(|&((z, color), size)| {
 ///
-///        // Extend each thread end w/ cut/finished yarn using pins from previous level for cutting.
-///        loom.extend_threads(zrow, color, size, &pins, &yarns);
+///        // Extend each thread end w/ finished yarn. Use pins for cutting.
+///        loom.extend_threads(yarns.finish(z, color, size, &pins));
 ///
-///        // Prepare the pins for the next level.
-///        pins = loom.pin_ends(zrow == LAST_ROW);
+///        // Prepare pins for next level. Stop pinning when zrow is the last.
+///        pins = loom.mark_next_ends(z);
 ///    });
 ///
 ///    // Mirror loom's threads to form subcycles from subchains for subsequent merging.
 ///    loom.mirror_threads();
 ///
 ///    // Split weft from the loom leaving only the warps.
-///    let (mut weft, mut loom) = loom.detach_weft(n);
+///    let (mut weft, mut loom) = loom.prepare_merging(n);
 ///
 ///    // Iterate over each warp in the loom and incorporate into the weft.
 ///    loom.iter_mut().for_each(|warp| {
@@ -62,7 +56,7 @@ use super::defs::*;
 ///        weft.join(warp);
 ///    });
 ///
-///    // Retrieve the finished weave.
+///    // After weaving there's is only the weft. Retrieve the finished weave.
 ///    weft.get_woven()
 ///    }
 ///```
@@ -72,17 +66,17 @@ use super::defs::*;
 /// For each level, get the requested color and cut. If there are pins in the pin pins, cut accordingly and finish.\
 /// Merge subcycles by first calculating the bridge between warp's and weft's edges: Align each sequence to their respective edge such that the two sequences can be placed next to another. Append the warp to the weaver's weft. Continue to incorporate warps into the weft until only the weft remains.
 ///
-///
+/// I've also abstracted away the
 pub fn weave(n: usize) -> Solution {
-    let yarns = Yarns::colorized(Spool::spin(n.get_spool_size()));
-    let mut pins: Cushion = Cushion::with_capacity(n);
-    let mut loom: Loom = Loom::with_capacity(n.get_loom_size());
-    n.get_zpos_size_color().iter().for_each(|&((zrow, size), color)| {
-        loom.extend_threads(zrow, color, size, &pins, &yarns);
-        pins = loom.pin_ends(zrow == LAST_ROW);
+    let mut pins = PinCushion::with_capacity(n);
+    let mut loom = Loom::with_capacity(n.loom_size());
+    let yarns = Yarns::colorized(Spindle::spin(n.spool_size()));
+    n.zrow_color_size().iter().for_each(|&((z, color), size)| {
+        loom.extend_threads(yarns.finish(z, color, size, &pins));
+        pins = loom.mark_next_ends(z);
     });
     loom.mirror_threads();
-    let (mut weft, mut loom) = loom.detach_weft(n);
+    let (mut weft, mut loom) = loom.prepare_merging(n);
     loom.iter_mut().for_each(|warp| {
         let warp_edges = warp.edges(weft.max_sum_z, weft.joined);
         let weft_bridge = weft.edges().bridge(&warp_edges);
