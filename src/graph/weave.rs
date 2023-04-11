6c1765f966b637272f0446b45945d85b144f8e48
www.discocube.org <93198518+discocube@users.noreply.github.com>
2023-04-11 16:12:47 +0200

    Update weave.rs

--- src/graph/weave.rs
+++ src/graph/weave.rs
@@ -7,7 +7,7 @@ use super::{defs::*, ops::prelude::*};
 /// `🧵 spun`: Spool of yarn to be colored.\
 /// `🧶 yarns`: Blue and red yarn as an ndarray.\
 /// `📌 pins`: Pins are used cut the finished yarn and as markers to connect the current level to the previous.\
-/// `🪜 loom`: Container onto which threads are woven level by level using pins as markers to connect the previous level to the current.\
+/// `🪜 loom`: Where threads are woven level by level using pins as markers to connect the prev to the current level.\
 /// `🪡 weft`: Leading loop into which the warps are incorporated. Weft object containing the solution.\
 /// `🧮 warps`: Threads which are built horizontally upwards level by level until half the graph has been built. \
 /// ---\
@@ -62,18 +62,18 @@ use super::{defs::*, ops::prelude::*};
 ///```
 /// ---\
 ///
-/// Spin and color yarn. From the bottom-up for each level: cut the yarn incorporate into the level using bobbins if necessary to affix to the previous threads. Prepare bobbins for the next level. When we've reached the top, reflect the loom. Separate the loom into a main weft and warps. Incorporate the weft into the warps. Return solution.\
+/// Spin and color yarn. From the bottom-up for each level: cut the yarn incorporate into the level using pins if necessary to affix to the previous threads. Prepare pins for the next level. When we've reached the top, reflect the loom. Separate the loom into a main weft and warps. Incorporate the weft into the warps. Return solution.\
 /// For each level, get the requested color and cut. If there are pins in the pin pins, cut accordingly and finish.\
 /// Merge subcycles by first calculating the bridge between warp's and weft's edges: Align each sequence to their respective edge such that the two sequences can be placed next to another. Append the warp to the weaver's weft. Continue to incorporate warps into the weft until only the weft remains.
 ///
-/// I've also abstracted away the
+/// I've placed most of the implementations in the ops.rs file to avoid cluttering the structure of the actual algorithm.
 pub fn weave(n: usize) -> Solution {
     let mut pins = PinCushion::with_capacity(n);
     let mut loom = Loom::with_capacity(n.loom_size());
     let yarns = Yarns::colorized(Spindle::spin(n.spool_size()));
-    n.zrow_color_size().iter().for_each(|&((z, color), size)| {
-        loom.extend_threads(yarns.finish(z, color, size, &pins));
-        pins = loom.mark_next_ends(z);
+    n.zrow_color_len().iter().for_each(|&((zrow, color), len)| {
+        loom.extend_threads(yarns.prep(zrow, color, len, &pins));
+        pins = loom.mark_next_ends(zrow);
     });
     loom.mirror_threads();
     let (mut weft, mut loom) = loom.prepare_merging(n);
