6ed7777a5bb8760dec0aa660314f289e7f8079e9
www.discocube.org <93198518+discocube@users.noreply.github.com>
2023-04-12 17:10:54 +0200

    Update weave.rs

--- src/graph/weave.rs
+++ src/graph/weave.rs
@@ -67,31 +67,34 @@ use super::{
 ///
 /// Spin and color yarn. From the bottom-up for each level: cut the yarn incorporate into the level using pins if necessary to affix to the previous threads. Prepare pins for the next level. When we've reached the top, reflect the loom. Separate the loom into a main weft and warps. Incorporate the weft into the warps. Return solution.\
 /// For each level, get the requested color and cut. If there are pins in the pin cushion, cut accordingly and finish.\
-/// Merge subcycles by first calculating the bridge between warp's and weft's edges: Align each sequence to their respective edge such that the two sequences can be placed next to another. Append the warp to the weaver's weft. Continue to incorporate warps into the weft until only the weft remains.
+/// Merge subcycles by first calculating the bridge between warp's and weft's edges: Align each sequence to their respective edge such that the two sequences can be placed next to another. Append the warp to the weaver's weft. Continue to incorporate warps into the weft until only the weft remains.\
+///---\
+/// ---\
+/// I've placed most of the implementations in the ops.rs file to avoid cluttering the structure of the actual algorithm. The ops.rs file is structured so that it follows the order of the weave algorithm, where each function is encapsulated in a separate module and imported using `prelude::*`. Here is a list of the modules with the corresponding call to that module in weave.\
+///```
+///graph_info_from_n::InfoN ->             n.loom_size()
+///                                        n.spool_size()
+///                                        n.zrow_color_len()
 ///
-/// I've placed most of the implementations in the ops.rs file to avoid cluttering the structure of the actual algorithm. The ops.rs file is structured so that it follows the order of the weave algorithm, where each function is encapsulated in a separate module and imported using `prelude::*`. Here is a list of the modules with the corresponding call to that module in weave.
+///spin_yarn::Spin ->                      Spindle::spin()
 ///
-///     graph_info_from_n::InfoN ->
-///         - PinCushion::with_capacity(n);
-///         - Loom::with_capacity(n.loom_size());
-///         - Yarns::colorized(Spindle::spin(n.spool_size()));
-///         - n.zrow_color_len()
-///     spin_yarn::Spin -> Spindle::spin(n.spool_size())
-///     color_spun_yarn::Convert -> Yarns::colorized(Spindle::spin(n.spool_size()));
-///     prep_yarn::PrepareYarn -> yarns.prep(zrow, color, len, &pins)
-///     extend_loom_threads::ExtendThreads -> loom.extend_threads(yarns.prep(zrow, color, len, &pins));
-///     mark_thread_ends::MarkThreadEnds -> pins = loom.mark_next_ends(zrow);
-///     mirror_loom_threads::Mirrored -> loom.mirror_threads();
-///     merge_cycles::*
-///         - let (mut weft, mut loom) = loom.prepare_cycle_merging(n);
-///           loom.iter_mut().for_each(|warp| {
-///               let warp_edges = warp.edges(weft.max_sum_z, weft.joined);
-///               let weft_bridge = weft.edges().bridge(&warp_edges);
-///               weft.data.align_to(weft_bridge);
-///               warp.align_to(warp_edges.bridge(&weft_bridge));
-///               weft.join(warp);
-///           });
+///color_spun_yarn::Convert ->             Yarns::colorized()
 ///
+///prep_yarn::PrepareYarn ->               yarns.prep()
+///
+///extend_loom_threads::ExtendThreads ->   loom.extend_threads()
+///
+///mark_thread_ends::MarkThreadEnds ->     loom.mark_next_ends()
+///
+///mirror_loom_threads::Mirrored ->        loom.mirror_threads()
+///
+///merge_cycles::* ->                      loom.prepare_cycle_merging()
+///                                        warp.edges()
+///                                        weft.edges().bridge(&warp_edges)
+///                                        weft.data.align_to(weft_bridge)
+///                                        warp.align_to(warp_edges.bridge(&weft_bridge))
+///                                        weft.join(warp)
+///```
 pub fn weave(n: usize) -> Solution {
     let mut pins = PinCushion::new_with_size(n);
     let mut loom = Loom::new_with_size(n.loom_size());
@@ -101,6 +104,7 @@ pub fn weave(n: usize) -> Solution {
         pins = loom.pin_threads_to_extend(zrow);
     });
     loom.mirror_threads();
+    println!("{loom:?}");
     let (mut weft, mut loom) = loom.prepare_cycle_merging(n);
     loom.iter_mut().for_each(|warp| {
         let warp_edges = warp.edges(weft.max_sum_z, weft.joined);
@@ -116,6 +120,7 @@ pub fn weave(n: usize) -> Solution {
 /// Test to check that the results from the first 50 orders are Hamiltonian cycles.
 mod tests {
     use crate::graph::ops::certify_solution::{Certify, SequenceID};
+
     use super::*;
 
     #[test]
